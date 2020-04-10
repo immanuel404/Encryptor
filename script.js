ae33f5bb696d7d5c362a6c2447645d9136f50080
Immanuel <57101108+immanuel404@users.noreply.github.com>
2020-04-10 10:32:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,108 @@
 
+	//CODE_ENCRYPTION
+	function process() {
+		var input = document.getElementById("encrypt").value;
+
+		// CODE TO PREVENT CHARACTER REPITION
+		if (/(.)\1{2,}/.test(input)) {
+		    alert("Dont Use Repeated characters!");
+  			exit()
+		}
+		// CHECKS LENGTH OF CHARACTER INPUTED
+  		var len = input.length;
+  		if (len < 6) {
+  			alert("Requires Minimum of 6 Characters");
+  			exit()
+  		}
+  		// CHECKS IF INPUT IS EMPTY
+		if (input == "") {
+			alert("Input Field is Empty");
+
+		// ELSE...
+		} else {
+			var str = input.split("");
+			var x;
+
+			for (x of str) {
+				// CONVERTS NUMBERS BY ADDING BY 1
+				if (x==1 || x==2 || x==3 || x==4 || x==5 || x==6 || x==7 || x==8) {
+					x = parseFloat(x) + parseFloat(1);
+				} else {}
+
+				// CONVERTS LETTERS TO VARIOUS SYMBOLS
+				if (x=="e") {
+					x = '!';
+				} else {}
+
+				if (x=="a") {
+					x = '@';
+				} else {}
+
+				if (x=="r") {
+					x = '#';
+				} else {}
+
+				if (x=="i") {
+					x = '$';
+				} else {}
+				
+				if (x=="o") {
+					x = '%';
+				} else {}
+
+				if (x=="t") {
+					x = '^';
+				} else {}
+
+				if (x=="n") {
+					x = '&';
+				} else {}
+
+				if (x=="s") {
+					x = '*';
+				} else {}
+				
+				if (x=="l") {
+					x = '-';
+				} else {}
+
+				if (x=="c") {
+					x = '+';
+				} else {}
+
+				if (x=="b") {
+					x = '~';
+				} else {}
+				
+				if (x=="d") {
+					x = '`';
+				} else {}
+
+				if (x=="f") {
+					x = '(';
+				} else {}
+
+				if (x=="g") {
+					x = ')';
+				} else {}
+
+				if (x=="h") {
+					x = '|';
+				} else {}
+				
+				if (x=="m") {
+					x = '?';
+				} else {}
+
+				if (x=="u") {
+					x = '>';
+				} else {}
+
+				document.write(x);
+			}
+		}
+	}	
+
 	// DECRYPTION CODE
 	function compute() {
 		var output = document.getElementById("decrypt").value;
